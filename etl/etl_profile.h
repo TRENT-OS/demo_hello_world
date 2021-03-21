b4366ff5bed91d098ad4549a28eb9443b968f8d7
Thomas Böhm <thomas.boehm@hensoldt-cyber.de>
2021-03-21 14:37:18 +0100

    add etl

---
+++ etl/etl_profile.h
@@ -0,0 +1,12 @@
+#ifndef __ETL_PROFILE_H__
+#define __ETL_PROFILE_H__
+
+#define ARDUINO
+#define ETL_NO_STL
+#define ETL_LOG_ERRORS
+#define ETL_VERBOSE_ERRORS
+#define ETL_CHECK_PUSH_POP
+
+#define ETL_TARGET_OS_FREERTOS // Placeholder. Not currently utilised in the ETL
+#define ETL_TARGET_DEVICE_ARM  // Placeholder. Not currently utilised in the ETL
+#endif
