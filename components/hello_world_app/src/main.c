be0a0b2d7bee7e6e3e98d1bbac4b8674d2b34d7c
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-06-23 16:22:59 +0200

    [TMP] axivion test code

--- components/hello_world_app/src/main.c
+++ components/hello_world_app/src/main.c
@@ -4,8 +4,37 @@
 
 #include <stdio.h>
 
-int run(void)
+static int testFunc(int i);
+
+int run()
 {
     printf("hello world!\n");
+
+    int i = 0;
+
+    while (i<10)
+    {
+        int value = testFunc(i);
+
+        if (value > 0)
+        {
+            i++;
+        }
+    }
+
     return 0;
+}
+
+int testFunc(int i)
+{
+    if (i % 2 == 0)
+    {
+        return i;
+    }
+    else
+    {
+        return 0;
+    }
+
+    return -1;
 }
